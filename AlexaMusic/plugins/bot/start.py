45cad4be120a12527e3176666e7ef991842d1e49
mmahsunaz-eng <mmahsun.az@gmail.com>
2025-10-23 11:47:59 +0700

    Update start.py

--- AlexaMusic/plugins/bot/start.py
+++ AlexaMusic/plugins/bot/start.py
@@ -186,23 +186,23 @@ async def start_comm(client, message: Message, _):
         except Exception:
             OWNER = None
         out = private_panel(_, app.username, OWNER)
-        if config.START_IMG_URL:
-            try:
-                await message.reply_photo(
-                    photo=config.START_IMG_URL,
-                    caption=_["start_2"].format(message.from_user.mention, app.mention),
-                    reply_markup=InlineKeyboardMarkup(out),
-                )
-            except Exception:
-                await message.reply_text(
-                    caption=_["start_2"].format(message.from_user.mention, app.mention),
-                    reply_markup=InlineKeyboardMarkup(out),
-                )
-        else:
-            await message.reply_text(
-                caption=_["start_2"].format(message.from_user.mention, app.mention),
-                reply_markup=InlineKeyboardMarkup(out),
-            )
+        if config.START_IMG_URL:  
+            try:  
+                await message.reply_photo(  
+                    photo=config.START_IMG_URL,  
+                    caption=_["start_2"].format(message.from_user.mention, app.mention),  
+                    reply_markup=InlineKeyboardMarkup(out),  
+                )  
+            except Exception:  
+                await message.reply_text(  
+                    _["start_2"].format(message.from_user.mention, app.mention),  
+                    reply_markup=InlineKeyboardMarkup(out),  
+                )  
+        else:  
+            await message.reply_text(  
+                _["start_2"].format(message.from_user.mention, app.mention),  
+                reply_markup=InlineKeyboardMarkup(out),  
+        )
         if await is_on_off(config.LOG):
             sender_id = message.from_user.id
             sender_name = message.from_user.first_name
